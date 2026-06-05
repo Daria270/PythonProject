db815f1ebdf62708fc22196487a84026834125c2
Daria270 <11061990d@gmail.com>
2026-06-05 23:34:38 +0300

    исправления по __pychache__

--- consumer.py
+++ consumer.py
@@ -2,12 +2,12 @@ import json
 from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(
-    topics="register-events",
-    bootstrap_servers=["185.185.143.231:8085"],
+    "register-events",
+    bootstrap_servers=["185.185.143.231:9092"],
     auto_offset_reset="earliest",
-    # value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+    value_deserializer=lambda x: json.loads(x.decode("utf-8")),
 )
 
 for message in consumer:
-    print(message.value)
+    print(message.value["login"])
 
